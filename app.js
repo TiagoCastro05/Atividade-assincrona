5e33212ac8594046a7aa7b294c8161ef968562c3
TIAGOCASTRO <tiago.c@ipvc.pt>
2025-06-09 13:30:08 +0100

    Commit 3; Logout

--- app.js
+++ app.js
@@ -1,81 +1,67 @@
-const { Console } = require('console');
-const expresss  = require('express');
-const session = require('express-session');
+const express = require('express');
+const session = require('express-session')
+
 const PORT = 3000;
 
-//Simular base de dados de utilizadores
-
+//simular base de dados de utilizadores
 
 bdusers = [
-    {username : "tiago", password : "1234", nickname : "Castro"},
-    {username : "Paulo", password : "123", nickname: "Paulinho"},
-]
+    { username: "tiago", password: "1234", nick: "Castro", color:"yellow"},
+    { username: "paulo", password: "54321" , nick : "paulinho", color:"lightblue"},
+    ];
 
-const app = new expresss();
-app.use(expresss.urlencoded())
-app.use(expresss.static('public'));
-app.use(session({secret: "1234"}));
+const app = new express();
+app.use(express.urlencoded());
+app.use(express.static('public'));
+app.use(session({ secret: "12345" }));
 
-app.post('/login', (req,res)=> {
+// configurar render engine de templates
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+// middleware de verificação de autenticação
+
+function estaAutenticado(req, res, next) {
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login.html')
+    }
+}
+
+app.get('/ola', estaAutenticado ,(req, res) => {
+    res.send("ola" +  " " + req.session.user)
+})
+    
+app.post('/login', (req, res) => {
     dadoslogin = req.body;
     console.log(dadoslogin);
-   const user =  bdusers.find((element) => element.username === dadoslogin.username)
-   
-   if(user && user.password === dadoslogin.password){
-    //Login com sucesso
-    req.session.user = user.username;
-    console.log("Login com sucesso");
-    res.redirect('/protected');
+    const user = bdusers.find((element) => element.username === dadoslogin.username)
+    if (user && user.password === dadoslogin.password) {
+        // login com sucesso
+        req.session.user = user.username;
+        req.session.nick = user.nick;
+        req.session.color = user.color;
+        console.log("sucesso")
+        res.redirect('/protected');
 
-   } else{
-    //falhou Login
-    console.log("Login falhou");
-    res.redirect('/login.html');
-   }
+    } else {
+        // falhou login
+        console.log("insucesso");
+        res.redirect('/login.html')
+    }
+});
 
-   
+app.get('/protected', estaAutenticado,(req, res) => {
+        res.render('viewprotected', { nick: req.session.nick , color: req.session.color});
+});
 
-
-
-   
-  /* let validlogin;
-    if(!user){
-        validlogin = false;
-    } else if (user.password === dadoslogin.password) {
-            validlogin = true;
-        } else {
-            validlogin = false;
-        }
-    
-
-     console.log(user + " : " + validlogin);
-
-
-
-     if(validlogin){
-    req.session.user = user.username;
-    req.redirect('/protected');
-   }else{
-    req.redirect('/login.html');
-   }
-*/
-
-   
-    
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
 })
-
-
-
-
-
-
-
 
 app.listen(PORT, () => {
-  console.log(`O servidor está a funcionar na porta: http://localhost:${PORT}`);
+    console.log("o servidor está a funcionar na porta : " + PORT);
 })
-
-
-
-
-
