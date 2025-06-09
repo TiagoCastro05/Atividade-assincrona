4f0793dbe6681ff4285405718304f7e7adc6dbaa
TIAGOCASTRO <tiago.c@ipvc.pt>
2025-06-09 12:02:51 +0100

    commit 2; Login

--- app.js
+++ app.js
@@ -1,25 +1,67 @@
+const { Console } = require('console');
 const expresss  = require('express');
+const session = require('express-session');
 const PORT = 3000;
 
 //Simular base de dados de utilizadores
+
+
 bdusers = [
-    {username : "tiago", password : "1234"},
-    {username : "Paulo", password : "123"},
-    
+    {username : "tiago", password : "1234", nickname : "Castro"},
+    {username : "Paulo", password : "123", nickname: "Paulinho"},
 ]
 
 const app = new expresss();
 app.use(expresss.urlencoded())
-
 app.use(expresss.static('public'));
-
-app.listen(PORT, () => {
-  console.log(`O servidor está a funcionar na porta: http://localhost:${PORT}`);
-})
+app.use(session({secret: "1234"}));
 
 app.post('/login', (req,res)=> {
     dadoslogin = req.body;
     console.log(dadoslogin);
+   const user =  bdusers.find((element) => element.username === dadoslogin.username)
+   
+   if(user && user.password === dadoslogin.password){
+    //Login com sucesso
+    req.session.user = user.username;
+    console.log("Login com sucesso");
+    res.redirect('/protected');
+
+   } else{
+    //falhou Login
+    console.log("Login falhou");
+    res.redirect('/login.html');
+   }
+
+   
+
+
+
+   
+  /* let validlogin;
+    if(!user){
+        validlogin = false;
+    } else if (user.password === dadoslogin.password) {
+            validlogin = true;
+        } else {
+            validlogin = false;
+        }
+    
+
+     console.log(user + " : " + validlogin);
+
+
+
+     if(validlogin){
+    req.session.user = user.username;
+    req.redirect('/protected');
+   }else{
+    req.redirect('/login.html');
+   }
+*/
+
+   
+    
 })
 
 
@@ -29,6 +71,9 @@ app.post('/login', (req,res)=> {
 
 
 
+app.listen(PORT, () => {
+  console.log(`O servidor está a funcionar na porta: http://localhost:${PORT}`);
+})
 
 
 
