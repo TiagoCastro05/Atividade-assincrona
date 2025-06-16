c29c1668e5955d7f7677cc50d23d2496ecd4292a
TIAGOCASTRO <tiago.c@ipvc.pt>
2025-06-16 12:24:11 +0100

    commit 4; Base de dados

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
-const session = require('express-session')
+const session = require('express-session');
+const dotenv = require('dotenv');
+const { MongoClient, ObjectId } = require('mongodb');
 
-const PORT = 3000;
+
+
+dotenv.config(); //Carrega variáveis de ambiente do ficheiro .env
+
+const PORT = process.env.PORT;
+
+
+//const SECRET = "12345";
+const SESSION_SECRET = process.env.SECRET || "12345"; // Variável de ambiente para a chave secreta da sessão
+
+
 
 //simular base de dados de utilizadores
 
@@ -34,9 +46,39 @@ app.get('/ola', estaAutenticado ,(req, res) => {
     res.send("ola" +  " " + req.session.user)
 })
     
-app.post('/login', (req, res) => {
+
+
+
+
+//login
+app.post('/login', async (req, res) => {
     dadoslogin = req.body;
     console.log(dadoslogin);
+
+    //Verifica se o utilizador existe na base de dados mongodb
+   user = usersCollection.findOne({ username: dadoslogin.username, password: dadoslogin.password })
+        .then(user => {
+            if (user) {
+
+                
+                // login com sucesso
+                req.session.user = user.username;
+                req.session.nick = user.nick;
+                req.session.color = user.color;
+                console.log("sucesso");
+                res.redirect('/protected');
+            } else {
+                // falhou login
+                console.log("insucesso");
+                res.redirect('/login.html');
+            }
+        })
+        .catch(err => {
+            console.error("Erro ao verificar usuário:", err);
+            res.redirect('/login.html');
+        });
+
+    /*
     const user = bdusers.find((element) => element.username === dadoslogin.username)
     if (user && user.password === dadoslogin.password) {
         // login com sucesso
@@ -50,7 +92,7 @@ app.post('/login', (req, res) => {
         // falhou login
         console.log("insucesso");
         res.redirect('/login.html')
-    }
+    }*/
 });
 
 app.get('/protected', estaAutenticado,(req, res) => {
@@ -62,6 +104,33 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 })
 
+
+//liga servidor
 app.listen(PORT, () => {
     console.log("o servidor está a funcionar na porta : " + PORT);
 })
+
+let db;
+let usersCollection;
+
+
+//liga bd e servidor
+async function startServer(){
+    try {
+        const client = new MongoClient(process.env.MONGOURL);
+        await client.connect();
+
+        db = client.db('UsersDB'); 
+        usersCollection = db.collection('Users'); 
+
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando na porta ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Erro ao conectar ao MongoDB:", error);
+    }
+}
+
+
+startServer();
