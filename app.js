aa3e8062b2234f51fc094857d46929947f594bf4
TIAGOCASTRO <tiago.c@ipvc.pt>
2025-06-22 14:34:57 +0100

    Commit 5; Registar contas e tornar mais bonito

--- app.js
+++ app.js
@@ -2,101 +2,82 @@ const express = require('express');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const { MongoClient, ObjectId } = require('mongodb');
+const bcrypt = require('bcrypt');
+const fetch = require('node-fetch'); // Instala: npm install node-fetch
 
 
 
-dotenv.config(); //Carrega variáveis de ambiente do ficheiro .env
+dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SECRET || "12345";
 
-
-//const SECRET = "12345";
-const SESSION_SECRET = process.env.SECRET || "12345"; // Variável de ambiente para a chave secreta da sessão
-
-
-
-//simular base de dados de utilizadores
-
-bdusers = [
-    { username: "tiago", password: "1234", nick: "Castro", color:"yellow"},
-    { username: "paulo", password: "54321" , nick : "paulinho", color:"lightblue"},
-    ];
-
-const app = new express();
-app.use(express.urlencoded());
+const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(session({ secret: "12345" }));
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
 
-// configurar render engine de templates
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+let db;
+let usersCollection;
+let historyCollection;
 
-// middleware de verificação de autenticação
-
+// Middleware de autenticação
 function estaAutenticado(req, res, next) {
     if (req.session.user) {
         next();
     } else {
-        res.redirect('/login.html')
+        res.redirect('/login.html');
     }
 }
 
-app.get('/ola', estaAutenticado ,(req, res) => {
-    res.send("ola" +  " " + req.session.user)
-})
-    
+// Rota de registo de utilizador
+app.get('/register', (req, res) => {
+    res.render('register'); // Cria views/register.ejs
+});
 
+app.post('/register', async (req, res) => {
+    const { username, password, nick, color } = req.body;
+    if (!username || !password) {
+        return res.send('Preenche todos os campos!');
+    }
+    const userExists = await usersCollection.findOne({ username });
+    if (userExists) {
+        return res.send('Utilizador já existe!');
+    }
+    const hash = await bcrypt.hash(password, 10);
+    await usersCollection.insertOne({ username, password: hash, nick, color });
+    res.redirect('/login.html');
+});
 
-
-
-//login
+// Login
 app.post('/login', async (req, res) => {
-    dadoslogin = req.body;
-    console.log(dadoslogin);
-
-    //Verifica se o utilizador existe na base de dados mongodb
-   user = usersCollection.findOne({ username: dadoslogin.username, password: dadoslogin.password })
-        .then(user => {
-            if (user) {
-
-                
-                // login com sucesso
-                req.session.user = user.username;
-                req.session.nick = user.nick;
-                req.session.color = user.color;
-                console.log("sucesso");
-                res.redirect('/protected');
-            } else {
-                // falhou login
-                console.log("insucesso");
-                res.redirect('/login.html');
-            }
-        })
-        .catch(err => {
-            console.error("Erro ao verificar usuário:", err);
-            res.redirect('/login.html');
-        });
-
-    /*
-    const user = bdusers.find((element) => element.username === dadoslogin.username)
-    if (user && user.password === dadoslogin.password) {
-        // login com sucesso
+    const { username, password } = req.body;
+    const user = await usersCollection.findOne({ username });
+    if (user && await bcrypt.compare(password, user.password)) {
         req.session.user = user.username;
         req.session.nick = user.nick;
         req.session.color = user.color;
-        console.log("sucesso")
         res.redirect('/protected');
-
     } else {
-        // falhou login
-        console.log("insucesso");
-        res.redirect('/login.html')
-    }*/
+        res.redirect('/login.html');
+    }
 });
 
-app.get('/protected', estaAutenticado,(req, res) => {
-        res.render('viewprotected', { nick: req.session.nick , color: req.session.color});
+// Página protegida
+app.get('/protected', estaAutenticado, async (req, res) => {
+    const history = await historyCollection.find({ username: req.session.user }).toArray();
+    res.render('viewprotected', { 
+        nick: req.session.nick, 
+        color: req.session.color, 
+        history // <-- esta linha garante que a variável é passada para a view
+    });
 });
 
 app.get('/logout', (req, res) => {
@@ -105,23 +86,53 @@ app.get('/logout', (req, res) => {
 })
 
 
-//liga servidor
-app.listen(PORT, () => {
-    console.log("o servidor está a funcionar na porta : " + PORT);
-})
+// Pesquisa (Mashup de APIs)
+app.get('/search', estaAutenticado, (req, res) => {
+    res.render('search'); // Cria views/search.ejs
+});
 
-let db;
-let usersCollection;
+app.post('/search', estaAutenticado, async (req, res) => {
+    const { termo } = req.body;
 
+    // Exemplo: OpenWeatherMap + Wikipedia
+    const weatherKey = process.env.OPENWEATHER_KEY;
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(termo)}&appid=${weatherKey}&units=metric&lang=pt`;
+    const wikiUrl = `https://pt.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(termo)}`;
 
-//liga bd e servidor
-async function startServer(){
+    let weatherData = {};
+    let wikiData = {};
+
+    try {
+        const weatherResp = await fetch(weatherUrl);
+        weatherData = await weatherResp.json();
+
+        const wikiResp = await fetch(wikiUrl);
+        wikiData = await wikiResp.json();
+
+        // Guarda histórico
+        await historyCollection.insertOne({
+            username: req.session.user,
+            termo,
+            data: new Date(),
+            weather: weatherData.weather ? weatherData.weather[0].description : 'N/A',
+            wiki: wikiData.extract || 'N/A'
+        });
+
+        res.render('result', { termo, weather: weatherData, wiki: wikiData });
+    } catch (err) {
+        res.send('Erro ao obter dados das APIs.');
+    }
+});
+
+// Liga à base de dados e inicia servidor
+async function startServer() {
     try {
         const client = new MongoClient(process.env.MONGOURL);
         await client.connect();
 
-        db = client.db('UsersDB'); 
-        usersCollection = db.collection('Users'); 
+        db = client.db('UsersDB');
+        usersCollection = db.collection('Users');
+        historyCollection = db.collection('History');
 
         app.listen(PORT, () => {
             console.log(`Servidor rodando na porta ${PORT}`);
@@ -131,6 +142,5 @@ async function startServer(){
         console.error("Erro ao conectar ao MongoDB:", error);
     }
 }
-
 
 startServer();
